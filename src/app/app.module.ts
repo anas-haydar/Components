ea93c304b89291a29b73d22b70fb0fe15d18a822
Anas haydar <ahaydar@osdservices.com>
2022-11-30 16:07:53 +0200

    dfs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,9 @@ import { ToggleSwitchComponent } from './toggle-switch/toggle-switch.component';
 import { AutoCompleteComponent } from './auto-complete/auto-complete.component';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ExactESignatureComponent } from './exact-e-signature/exact-e-signature.component';
 
 @NgModule({
-  declarations: [AppComponent, AutoCompleteComponent, ExactESignatureComponent],
+  declarations: [AppComponent, AutoCompleteComponent],
   imports: [
     BrowserModule,
     NgMultiSelectDropDownModule.forRoot(),
@@ -20,8 +19,5 @@ import { ExactESignatureComponent } from './exact-e-signature/exact-e-signature.
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [
-    ExactESignatureComponent
-  ],
 })
 export class AppModule {}
